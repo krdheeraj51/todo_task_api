8fc9b25c1dd802da1ec8dbec1f05b031b61106f2
krdheeraj51 <krdheeraj51@gmail.com>
2022-08-23 19:18:28 +0530

    changes for removing body parser with express.

--- index.js
+++ index.js
@@ -1,38 +1,35 @@
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const appConfig = require('./config/appConfig');
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser');
+const mongoose = require("mongoose");
+const appConfig = require("./config/appConfig");
+const cookieParser = require("cookie-parser");
 
 //middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //model
-require('./model/User');
-require('./model/Task');
-require('./model/SubTask');
-const appRouter = require('./route/appRoute')
+require("./model/User");
+require("./model/Task");
+require("./model/SubTask");
+const appRouter = require("./route/appRoute");
 //call router
 appRouter.setAppRouter(app);
 
 app.listen(appConfig.port, () => {
-
-    let db = mongoose.connect(appConfig.db.uri, { useNewUrlParser: true });
-
-})
+  let db = mongoose.connect(appConfig.db.uri, { useNewUrlParser: true });
+});
 //handling mongoose connection error
-mongoose.connection.on('error', (err) => {
-    console.log('DataBase connection error :::');
-})
+mongoose.connection.on("error", (err) => {
+  console.log("DataBase connection error :::");
+});
 console.log("after connection error ....");
-mongoose.connection.on('open', (err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('DBConnection Established Successfully .....');
-    }
-
-})
+mongoose.connection.on("open", (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("DBConnection Established Successfully .....");
+  }
+});
